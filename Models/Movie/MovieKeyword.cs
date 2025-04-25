6fe094d49bf69d3c389369e7f042fb1fc4a582c5
Ventura <ventura.py3@gmail.com>
2025-04-25 20:24:16 -0300

    feat: Adiciona início da modelagem dos filmes

---
+++ Models/Movie/MovieKeyword.cs
@@ -0,0 +1,11 @@
+﻿namespace LumeServer.Models.Movie
+{
+    public class MovieKeyword
+    {
+        public string MovieId { get; set; }
+        public int KeywordId { get; set; }
+
+        public Movie Movie { get; set; }
+        public Keyword Keyword { get; set; }
+    }
+}
---
+++ Models/Movie/MovieSpokenLanguage.cs
@@ -0,0 +1,11 @@
+﻿namespace LumeServer.Models.Movie
+{
+    public class MovieSpokenLanguage
+    {
+        public string MovieId { get; set; }
+        public string SpokenLanguageId { get; set; }
+
+        public Movie Movie { get; set; }
+        public SpokenLanguage SpokenLanguage { get; set; }
+    }
+}
