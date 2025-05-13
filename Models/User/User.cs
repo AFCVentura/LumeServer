e8f95290a36a058b31e3f2871d11359543194285
Ventura <ventura.py3@gmail.com>
2025-05-13 17:18:26 -0300

    feat: Adiciona preferências extras pro perfil geral e perfil do momento do usuário

--- Models/User/User.cs
+++ Models/User/User.cs
@@ -5,6 +5,13 @@ namespace LumeServer.Models.User
     // Esse é o Model do usuário, ele apenas extende a classe IdentityUser, que já tem todos os atributos necessários para um usuário.
     public class User : IdentityUser
     {
+        public float MinVoteAverage { get; set; }
+        public float MaxVoteAverage { get; set; }
+        public int MinVoteCount { get; set; }
+        public int MaxVoteCount { get; set; }
+        public int MinYear { get; set; }
+        public int MaxYear { get; set; }
+
         public ICollection<UserDailyProfile> DailyProfiles { get; set; }
         public ICollection<UserGeneralProfileCluster> GeneralProfileClusters { get; set; }
         public ICollection<WatchedList> WatchedList { get; set; }
--- Models/User/UserDailyProfile.cs
+++ Models/User/UserDailyProfile.cs
@@ -5,6 +5,12 @@
         public int Id { get; set; }
         public string UserId { get; set; }
         public User User { get; set; }
+        public float MinVoteAverage { get; set; }
+        public float MaxVoteAverage { get; set; }
+        public int MinVoteCount { get; set; }
+        public int MaxVoteCount { get; set; }
+        public int MinYear { get; set; }
+        public int MaxYear { get; set; }
         public DateTime Timestamp { get; set; }
         public bool IsTheLatest { get; set; }
         public ICollection<UserDailyProfileCluster> UserDailyProfileClusters { get; set; }
